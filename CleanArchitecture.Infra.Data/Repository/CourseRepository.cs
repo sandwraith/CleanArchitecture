088c4d877911061f756b21c7b05b4083b2ee0377
boris_home <bselayet@maritimetrainer.com>
2020-01-18 00:32:11 +0300

    Commit implementation of Service and Repository GetCourses Methods

---
+++ CleanArchitecture.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using CleanArchitecture.Infra.Data.Context;
+
+namespace CleanArchitecture.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _universityDbContext;
+
+        public CourseRepository(UniversityDbContext universityDbContext)
+        {
+            _universityDbContext = universityDbContext;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _universityDbContext.Courses;
+        }
+    }
+}
