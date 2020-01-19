79fe1c8aab55eba12ec7db7e08b5e652a46b5c7e
Barış Selayet <bselayet@maritimetrainer.com>
2020-01-19 23:02:24 +0300

    Adding Domain.Core and implemetation of InMemoryBus

---
+++ CleanArchitecture.Domain.Core/Commands/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArchitecture.Domain.Core.Events;
+
+namespace CleanArchitecture.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
